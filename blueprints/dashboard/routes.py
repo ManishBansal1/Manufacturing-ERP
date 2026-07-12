f89e17b0b603f05538726aabbfc2f3ef68f25f4d
Manish Bansal <bansalmanish3003@gmail.com>
2026-07-13 02:48:00 +0530

    Added Dashboard for Receivable 13.07.2026

--- blueprints/dashboard/routes.py
+++ blueprints/dashboard/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 from datetime import date
@@ -20,6 +20,7 @@ from models import (
     BillSubmissionDetail,
     PaymentReceiptHeader,
     CustomerMaster,
+    ItemMaster,
     db
 
 )
@@ -42,6 +43,21 @@ def outstanding_receipts_dashboard():
 
     today = date.today()
 
+    selected_fy = request.args.get(
+    "financial_year",
+    type=int
+    )
+
+    if not selected_fy:
+
+        if today.month >= 4:
+
+            selected_fy = today.year + 1
+
+        else:
+
+            selected_fy = today.year
+
     if today.month >= 4:
         current_fy = today.year + 1
     else:
@@ -52,11 +68,139 @@ def outstanding_receipts_dashboard():
     for fy in range(current_fy, current_fy - 6, -1):
         financial_years.append(fy)
 
-    invoices = (
+    query = SalesInvoiceHeader.query.filter(
+
+    SalesInvoiceHeader.status != "CANCELLED"
+
+    )
+
+    fy_start = date(
+
+    selected_fy - 1,
+
+    4,
+
+    1
+
+    )
+
+    fy_end = date(
+
+        selected_fy,
+
+        3,
+
+        31
+
+    )
+
+    from_date = request.args.get("from_date")
+
+    if from_date:
+
+        from_date = date.fromisoformat(from_date)
+
+    to_date = request.args.get("to_date")
+
+    if to_date:
+
+        to_date = date.fromisoformat(to_date)
+
+    invoice_status = request.args.get("invoice_status")
+
+    item_name = request.args.get("item_name")
+
+    customer_name = request.args.get("customer_name")
+
+    query = query.filter(
+
+        SalesInvoiceHeader.invoice_date >= fy_start,
+
+        SalesInvoiceHeader.invoice_date <= fy_end
+
+    )
+
+    if from_date:
+
+        query = query.filter(
+
+            SalesInvoiceHeader.invoice_date >= from_date
+
+        )
+
+    if to_date:
+
+        query = query.filter(
+
+            SalesInvoiceHeader.invoice_date <= to_date
+
+        )
+
+    if invoice_status and invoice_status != "ALL":
+
+        query = query.filter(
+
+            SalesInvoiceHeader.status == invoice_status
+
+        )
+
+    if item_name and item_name != "ALL":
+
+        query = query.join(
+
+            SalesInvoiceDetail
+
+        ).join(
+
+            ItemMaster
+
+        ).filter(
+
+            ItemMaster.item_name == item_name
+
+        )
+
+    
+    if customer_name and customer_name != "ALL":
+
+        query = query.join(
+
+            CustomerMaster
+
+        ).filter(
+
+            CustomerMaster.customer_name == customer_name
+
+        )
+
+    customer_list = (
+
+        CustomerMaster.query
+
+        .order_by(
+
+            CustomerMaster.customer_name
+
+        )
+
+        .all()
+
+    )
+
+
+
+    invoices = query.order_by(
+
+    SalesInvoiceHeader.invoice_date.desc()
+
+    ).all()
+
+    ''' invoices = (
         SalesInvoiceHeader.query
+        .filter(SalesInvoiceHeader.status != "CANCELLED")
         .order_by(SalesInvoiceHeader.invoice_date.desc())
         .all()
-    )
+    )'''
 
     receivable_rows = []
 
@@ -141,6 +285,32 @@ def outstanding_receipts_dashboard():
                 .first()
             )
 
+        pending_amount = (
+            (bill.bill_amount if bill else 0)
+            -
+            (payment.amount_received if payment else 0)
+            -
+            (payment.tds_deducted if payment else 0)
+            -
+            (payment.ld_charges if payment else 0)
+            -
+            (payment.general_damage_charges if payment else 0)
+            -
+            (payment.other_deductions if payment else 0)
+        )
+
+        if invoice.status == "Payment Received" and payment:
+
+            collection_days = (
+                payment.payment_date - invoice.invoice_date
+            ).days
+
+        else:
+
+            collection_days = (
+                today - invoice.invoice_date
+            ).days
+
         receivable_rows.append({
 
             "customer_po_no":
@@ -203,10 +373,160 @@ def outstanding_receipts_dashboard():
             "other_deductions":
                 payment.other_deductions if payment else 0,
 
+            "pending_amount":
+                pending_amount,
+
+            "collection_days":
+                collection_days,
+
             "payment_status":
                 payment.status if payment else ""
 
         })
+
+    grand_totals = {
+
+        "dispatch_qty": 0,
+
+        "received_qty": 0,
+
+        "bill_amount": 0,
+
+        "amount_received": 0,
+
+        "tds": 0,
+
+        "ld": 0,
+
+        "general_damage": 0,
+
+        "other_deductions": 0,
+
+        "pending_amount": 0
+
+    }
+
+    for row in receivable_rows:
+
+        grand_totals["dispatch_qty"] += row["dispatch_qty"]
+
+        grand_totals["received_qty"] += row["received_qty"]
+
+        grand_totals["bill_amount"] += row["bill_amount"]
+
+        grand_totals["amount_received"] += row["amount_received"]
+
+        grand_totals["tds"] += row["tds_deducted"]
+
+        grand_totals["ld"] += row["ld_charges"]
+
+        grand_totals["general_damage"] += row["general_damage_charges"]
+
+        grand_totals["other_deductions"] += row["other_deductions"]
+
+        grand_totals["pending_amount"] += row["pending_amount"]
+
+    item_list = (
+
+        db.session.query(
+
+            ItemMaster.item_name
+
+        )
+
+        .join(
+            SalesInvoiceDetail,
+
+            SalesInvoiceDetail.item_id == ItemMaster.id
+
+        )
+
+        .filter(
+            ItemMaster.item_type == "Finished Goods"
+
+        )
+
+        .distinct()
+
+        .order_by(
+
+            ItemMaster.item_name
+
+        )
+
+        .all()
+
+    )
+
+
+    summary = {
+
+        "outstanding_amount": 0,
+
+        "pending_bills": 0,
+
+        "average_collection_days": 0,
+
+        "oldest_outstanding": 0
+
+    }
+
+    payment_days_total = 0
+
+    payment_invoice_count = 0
+
+    for row in receivable_rows:
+
+        # Outstanding Amount
+        summary["outstanding_amount"] += row["pending_amount"]
+
+        # Bills Pending
+        if row["invoice_status"] not in (
+
+            "Payment Received",
+
+            "Cancelled"
+
+        ):
+
+            summary["pending_bills"] += 1
+
+        # Average Collection Days
+        if row["invoice_status"] == "Payment Received":
+
+            payment_days_total += row["collection_days"]
+
+            payment_invoice_count += 1
+
+        # Oldest Outstanding
+        if row["invoice_status"] not in (
+
+            "Payment Received",
+
+            "Cancelled"
+
+        ):
+
+            if row["collection_days"] > summary["oldest_outstanding"]:
+
+                summary["oldest_outstanding"] = row["collection_days"]
+
+    if payment_invoice_count:
+
+        summary["average_collection_days"] = round(
+
+            payment_days_total /
+
+            payment_invoice_count,
+
+            1
+
+        )
+
+    else:
+
+        summary["average_collection_days"] = 0
+
 
     return render_template(
 
@@ -214,8 +534,26 @@ def outstanding_receipts_dashboard():
 
         financial_years=financial_years,
 
-        current_fy=current_fy,
+        selected_fy=selected_fy,
 
-        receivable_rows=receivable_rows
+        from_date=from_date,
+
+        to_date=to_date,
+
+        invoice_status=invoice_status,
+
+        item_list=item_list,
+
+        item_name=item_name,
+
+        customer_list=customer_list,
+
+        customer_name=customer_name,
+
+        receivable_rows=receivable_rows,
+
+        grand_totals=grand_totals,
+        
+        summary=summary
 
     )
